a6290f4bcfd58187fe923c9ba6d6373680ad7bdb
TomMacard <tom.macard@gmail.com>
2023-10-27 15:14:20 +0200

    ia du jeu

--- ia.py
+++ ia.py
@@ -1,3 +1,70 @@
+from hanoi import *
+
+def choix_ia():
+    while True:
+        try:
+            entier = int(input("Voulez-vous laisser l'IA jouer? 1=oui, 0=non : "))
+            if entier==1 or entier ==0:
+                return entier
+            else:
+                print("Entrer soit 0 soit 1 !")
+        except ValueError:
+            print("Veuillez entrer un entier !")
+
+
+def action_ia(jeu,action,marqueur):
+    p1=0
+    p2=0
+    p3=0
+    i=0
+    while p1==0:
+        p1=jeu.piquet1[i]
+        i=i+1
+    i=0
+    while p2==0:
+        p2=jeu.piquet2[i]
+        i=i+1
+    i=0
+    while p3==0:
+        p3=jeu.piquet3[i]
+        i=i+1
+
+    #print(p1,p2,p3)
+    
+    #cas initial
+    if marqueur[0]==0:
+        return((1,2),(23,True))
+
+    #cas possible si rotation du plus petit a été effectuée
+    if marqueur[1]:
+        if action==(1,2):
+            if p1<p3:
+                return((1,3),(23,False))
+            else:
+                return((3,1),(23,False))
+        if action==(2,3):
+            if p1<p2:
+                return((1,2),(31,False))
+            else:
+                return((2,1),(31,False))
+            
+        if action==(3,1):
+            if p3<p2:
+                return((3,2),(12,False))
+            else:
+                return((2,3),(12,False))
+    
+    #rotation du plus petit
+    if marqueur[0]==12:
+        return((1,2),(23,True))
+    if marqueur[0]==23:
+        return((2,3),(31,True))
+    if marqueur[0]==31:
+        return((3,1),(12,True))
 
 
 
+        
+ 
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from hanoi import *
+from ia import *
+import time
 
 debut()
 niveau = choix_de_niveau()
+
+choixia= choix_ia()
+marqueur=(0,0) # 1er chiffre: dernier mouvement du petit, 2eme : si le dernier mouvement etait le petit
+action=(0,0)
+
 jeu = TourHanoi(niveau)
 jeu.dessine_jeu()
 
@@ -9,7 +16,11 @@ continuer = True
 
 while continuer :
 
-    action = action_utilisateur()
+    if not choixia:
+        action = action_utilisateur()
+    else:
+        (action,marqueur) = action_ia(jeu,action,marqueur)
+        time.sleep(0.7)
 
     if action[0]==0 and action[1]==0 :
         continuer = False
